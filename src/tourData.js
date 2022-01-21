dcd249272461777f0668259ad900c74f03e06852
baboomerang <irodrigue101@gmail.com>
2022-01-20 20:38:10 -0500

    added base project to the repo

---
+++ src/tourData.js
@@ -0,0 +1,51 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "new york",
+        img: "./img/newyork.jpg",
+        name: "Statue of Liberty tour",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+    },
+    {
+        id: 2,
+        city: "paris",
+        img: "./img/paris.jpeg",
+        name: "Paris Lights tour",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+        
+    },
+    {
+        id: 3,
+        city: "london",
+        img: "./img/london.jpeg",
+        name: "london royal palace tour",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+    },
+    {
+        id: 4,
+        city: "tokyo",
+        img: "./img/tokyo.jpeg",
+        name: "tokyo sushi tour",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+    },
+    {
+        id: 5,
+        city: "Brooklyn Bridge",
+        img: "./img/bkbridge.jpg",
+        name: "Brooklyn Bridge",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+    },
+    {
+        id: 6,
+        city: "Yankee Stadium",
+        img: "./img/yankeestadium.jpg",
+        name: "Yankee Stadium",
+        info: 
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry." 
+    },
+];
